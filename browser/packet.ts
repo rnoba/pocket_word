08f2e16e8369663742d722cbcec0257a628b0571
pnf(rnoba) <rnoba.iwb@gmail.com>
2025-01-24 23:08:50 -0300

    client

--- browser/packet.ts
+++ browser/packet.ts
@@ -136,7 +136,20 @@ export enum PacketKind {
 	PacketKind_Ping								= 1 << 1,
 	PacketKind_Pong								= 1 << 2,
 	PacketKind_RequestSpriteInfo	= 1 << 3,
+	PacketKind_AuthenticationClient	= 1 << 4,
+	PacketKind_Ok										= 1 << 5,
+	PacketKind_AuthenticationServer	= 1 << 6,
 }
+
+export const packet_kind_to_string = {
+	[PacketKind.PacketKind_SpriteInfo]: "<SPRITE INFO>",
+	[PacketKind.PacketKind_Ping]: "<PING>",	
+	[PacketKind.PacketKind_Pong]: "<PONG>",	
+	[PacketKind.PacketKind_RequestSpriteInfo]: "<REQUEST SPRITE INFO>",
+	[PacketKind.PacketKind_AuthenticationClient]: "<AUTHENTICATION CLIENT>",
+	[PacketKind.PacketKind_Ok]: "<OK>",	
+	[PacketKind.PacketKind_AuthenticationServer]: "<AUTHENTICATION SERVER>"
+} as Record<PacketKind, string>; 
 
 enum AgentKind {
 	AgentKind_Server				= 1 << 0,
@@ -162,6 +175,33 @@ function agent_size(agent: Agent) {
 type PacketPing = {
 	length: number;
 	msg:	string;
+}
+
+export type PacketOk = {
+	ok: number;
+	ctx: PacketKind;
+}
+
+type PacketAuthenticationClient = {
+	// u32
+	username_len: number;
+	username: string;
+	// u32
+	password_len: number;
+	password: string;
+}
+
+export type PacketAuthenticationServer = {
+	//u64;
+	user_id: number;
+	//u8
+	is_admin: number;
+	// u32
+	username_len: number;
+	username: string;
+	//u32
+	created_at_len: number;
+	created_at: string;
 }
 
 type PacketPong = {
@@ -199,7 +239,13 @@ export type Packet = {
 	// the packet in the server 
 	kind: number;
 	agent: Agent;
-	payload: PacketRequestSpriteInfo | PacketSpriteInfo | PacketPong | PacketPing;
+	payload: PacketAuthenticationClient |
+					 PacketRequestSpriteInfo |
+					 PacketSpriteInfo |
+					 PacketAuthenticationServer |
+					 PacketPong |
+					 PacketOk |
+					 PacketPing;
 }
 
 // id 0 -> reserverd for anonymous agents 
@@ -238,6 +284,22 @@ export function packet_pong_make()
 	} as PacketPing
 	packet.size = 8 + agent_size(packet.agent) + PingPongSize;
 	return (packet);
+}
+
+export function packet_authentication_client_make(username: string, password: string): Packet
+{
+	const packet: Packet = {} as Packet;
+	packet.agent = agent_anonymous_make();
+	packet.kind = PacketKind.PacketKind_AuthenticationClient;
+	packet.payload = {
+		username_len: username.length,
+		username,
+		password_len: password.length,
+		password
+	} as PacketAuthenticationClient; 
+	packet.size = 8 + agent_size(packet.agent) + packet.payload.password_len +
+		packet.payload.username_len + 8;
+	return packet;
 }
 
 export const PingPacket = packet_ping_make(); 
@@ -304,6 +366,13 @@ export function packet_serialize(packet: Packet): ArrayBuffer
 			offset = write_u32(dv, offset, payload.name_length);
 			write_s8(dv, offset, payload.name);
 		} break;
+		case PacketKind.PacketKind_AuthenticationClient: {
+			const payload = (packet.payload) as PacketAuthenticationClient;
+			offset = write_u32(dv, offset, payload.username_len);
+			offset = write_s8(dv, offset, payload.username);
+			offset = write_u32(dv, offset, payload.password_len);
+			write_s8(dv, offset, payload.password);
+		}
 		default: {
 		} break;
 	}
@@ -332,6 +401,25 @@ export function packet_pongping_deserialize(dv: DataView, offset = 0): PacketPon
 	const [length, offseta]	= read_u32(dv, offset); 
 	const [msg,		_]				= read_s8(dv, offseta, length);
 	return { msg, length };
+}
+
+export function packet_authentication_server_deserialize(dv: DataView, offset = 0): PacketAuthenticationServer
+{
+	const [user_id, offseta]	= read_u64(dv, offset); 
+	const [is_admin, offsetb]	= read_u8(dv, offseta);
+	const [username_len, offsetc]	= read_u32(dv, offsetb);
+	const [username, offsetd]	= read_s8(dv, offsetc, username_len);
+	const [created_at_len, offsete]	= read_u32(dv, offsetd);
+	const [created_at, _]	= read_s8(dv, offsete, created_at_len);
+
+	return {
+		user_id,
+		is_admin,
+		username_len,
+		username,
+		created_at_len,
+		created_at
+	} as PacketAuthenticationServer;
 }
 
 export function packet_sprite_info_deserialize(dv: DataView, offset = 0): PacketSpriteInfo
@@ -376,16 +464,32 @@ export function packet_deserialize(data: ArrayBuffer): Packet
 	packet.size		= dv.getUint32(0, true);
 	packet.kind		= dv.getUint32(4, true);
 	packet.agent	= agent_deserialize(dv, 8); 
+
+	const offset = 8+agent_size(packet.agent);
 	switch (packet.kind)
 	{
 		case PacketKind.PacketKind_Ping:
 		case PacketKind.PacketKind_Pong: {
-			packet.payload = packet_pongping_deserialize(dv, 8+agent_size(packet.agent));
+			packet.payload = packet_pongping_deserialize(dv, offset);
 		} break;
 		case PacketKind.PacketKind_SpriteInfo:
 		{
-			packet.payload = packet_sprite_info_deserialize(dv, 8+agent_size(packet.agent));
+			packet.payload = packet_sprite_info_deserialize(dv, offset);
 		} break;
+		case PacketKind.PacketKind_Ok:
+		{
+			const [ok,	offseta] = read_u8(dv, offset); 
+			const [ctx, _]	= read_u32(dv, offseta);
+			const payload = {
+				ok,
+				ctx
+			} as PacketOk;
+			packet.payload = payload;
+		} break;
+		case PacketKind.PacketKind_AuthenticationServer:
+		{
+			packet.payload = packet_authentication_server_deserialize(dv, offset);
+		}
 		default: {
 		} break;
 	}
